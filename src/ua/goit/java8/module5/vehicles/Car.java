36d9e677d3577bf9bdcddb9abf0694b2b55b525c
TarasOleksiv <toleksiv69@gmail.com>
2017-07-20 17:52:32 +0300

    Add several methods to class 'Car'

--- src/ua/goit/java8/module5/vehicles/Car.java
+++ src/ua/goit/java8/module5/vehicles/Car.java
@@ -10,12 +10,15 @@ public class Car {
     private final int releaseDate;    //дата виробництва, не може бути змінена після створення об'єкта
     private String engineType;      //тип двигуна
     private int maxSpeed;       //максимальна швидкість для нової машини
-    private int runupTime;      //час розгону до 100км.год
+    private int runupTime;      //час розгону до 100км/год
     private int totalNumberOfPassengers;    //вмістимість пасажирів
     private int currentNumberOfPassengers;  //кількість пасажирів в поточний момент
     private int currentSpeed;       //поточна швидкість
-    private CarDoor[] cardoor = new CarDoor[4];     //масив з 5 дверей
-    private CarWheel[] carwheel = new CarWheel[3];      //масив з 4 коліс
+    private CarDoor[] cardoors = new CarDoor[MAX_DOORS - 1];     //масив з 4 дверей
+    private CarWheel[] carwheels = new CarWheel[MAX_WHEELS - 1];      //масив з 10 коліс
+    private int wheelCounter;       //поточна кількість коліс на машині
+    private static final int MAX_DOORS = 4;     //максимальна кількість дверей
+    private static final int MAX_WHEELS = 10;     //максимальна кількість дверей
 
     // constructor1
     public Car(int releaseDate) {
@@ -76,5 +79,84 @@ public class Car {
         }
     }
 
+    //ініціалізація дверей
+    public void setCarDoors(){
+        for (CarDoor cardoor : cardoors){
+            cardoor = new CarDoor();
+        }
+    }
 
+    //ініціалізація коліс
+    public void setCarWheels(){
+        wheelCounter = 4;
+        for (int i=0; i <=3; i++){
+            carwheels[i] = new CarWheel();
+        }
+    }
+
+    //отримати двері по індексу
+    public CarDoor getCarDoor(int i){
+        return cardoors[i];
+    }
+
+    //отримати колесо по індексу
+    public CarWheel getCarWheel(int i){
+        return carwheels[i];
+    }
+
+    //зняти всі колеса з машини
+    public void getOffAllCarWheels(){
+        for (CarWheel carwheel: carwheels){
+            carwheel = null;
+        }
+        wheelCounter = 0;
+    }
+
+    //встановити на машину X нових коліс
+    public void setNewWheels(int x){
+        if (x + wheelCounter > MAX_WHEELS) {
+            System.out.println("Неможливо додати " + x + " коліс. Буде перевищено максимальну кількість " + MAX_WHEELS);
+        } else {
+            for (int i = 0; i < x; i++){
+                carwheels[wheelCounter + i] = new CarWheel();
+            }
+            wheelCounter += x;
+        }
+    }
+
+    //обчислити поточну можливу максимальну швидкість
+    public double getCurrentMaxSpeed(){
+        if ((currentNumberOfPassengers == 0) || (wheelCounter == 0)){
+            return 0;
+        } else {
+            return maxSpeed * getMinTireWheel();
+        }
+    }
+
+    //отримати найбільш стерте колесо
+    private double getMinTireWheel(){
+        double result = carwheels[0].getTireState();
+        for (int i = 0; i < wheelCounter; i++){
+            result = (carwheels[i].getTireState() < result)?carwheels[i].getTireState():result;
+        }
+        return result;
+    }
+
+    //отримати поточну кількість коліс
+    public int getCurrentNumberOfWheels(){
+        return wheelCounter;
+    }
+
+    //вивести всю інфу на консоль
+    public void showinfo(){
+        System.out.println("Дата виробництва машини: " + releaseDate);
+        System.out.println("Тип двигуна: " + engineType);
+        System.out.println("Максимальна швидкість нової машини: " + maxSpeed);
+        System.out.println("Час розгону до 100км/год: " + runupTime);
+        System.out.println("Вмістимість пасажирів: " + totalNumberOfPassengers);
+        System.out.println("Кількість пасажирів на даний момент: " + currentNumberOfPassengers);
+        System.out.println("Поточна кількість коліс: " + wheelCounter);
+        System.out.println("Поточна швидкість: " + currentSpeed);
+        System.out.println("Поточна можлива максимальна швидкість: " + getCurrentMaxSpeed());
+    }
 }
